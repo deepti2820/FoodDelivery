5c86604ce8f8242efd080a5b15741a6bed98a36f
deepti2820 <165156846+deepti2820@users.noreply.github.com>
2024-09-09 00:21:18 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 
 module.exports.placeOrder=async(req,res)=>{
   
-     const frontend_url="http://localhost:5174"
+     const frontend_url=process.env.BASE_URL;
 
     try{
         const newOrder=new orderModel({
